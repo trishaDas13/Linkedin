c27a2c1eb6b2700c02e4d9eb29006f1ab190dfaa
trishaDas13 <trisha.das1308@gmail.com>
2024-02-09 02:02:02 +0530

    home page profile created

---
+++ src/components/common/homeProfileBar/HomeProfileBar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './style.scss';
+import { useNavigate } from 'react-router-dom';
+
+
+const HomeProfileBar = ({currentUser}) => {
+    const navigate = useNavigate();
+
+    const navigateToProfile = () =>{
+        navigate('/profile');
+    }
+  return (
+    <div className='home_profile_bar'>
+        <div className="bgImage">
+            <img src={currentUser.coverLink} alt="backgroundImage" className='backgroundImage'  />
+        </div>
+        <div className="conact_content">
+            <img src={currentUser.profileLink} alt="" />
+            <div className="names">
+            <h4 onClick={navigateToProfile}>Hi, I’m {currentUser.name}!</h4>
+            <p>{currentUser.headline}</p>
+            </div>
+            <hr />
+            <h3>Connect with me on:</h3>
+            <ul>
+            <a href={currentUser.contact1} target='_blank'><li>Instagram</li></a>
+            <a href={currentUser.contact3} target='_blank'><li>Linked in</li></a>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default HomeProfileBar
--- src/components/homeCtx/HomeCtx.jsx
+++ src/components/homeCtx/HomeCtx.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Topbar from '../common/topbar/Topbar';
 import PostUpdate from '../common/postUpdate/PostUpdate';
-import './style.scss'
+import './style.scss';
+import HomeProfileBar from '../common/homeProfileBar/HomeProfileBar';
+
 
 
 const HomeCtx = ({currentUser}) => {
   
   return (
     <div className='homePage'>
+      <HomeProfileBar  currentUser={currentUser}/>
       <PostUpdate currentUser={currentUser}/>
+      
       </div>
   )
 }
