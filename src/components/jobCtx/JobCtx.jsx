62de0db85cf6948337bc030eafdbb5cd940fb5e9
trishaDas13 <trisha.das1308@gmail.com>
2024-02-24 12:38:43 +0530

    job page done

--- src/components/jobCtx/JobCtx.jsx
+++ src/components/jobCtx/JobCtx.jsx
@@ -1,32 +1,76 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './style.scss'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./style.scss";
 
 const JobCtx = () => {
+  const [jobs, setJobs] = useState([]);
+  const[page, setPage] = useState(0);
 
-    const[jobs, setJobs] = useState([]); //array of jobs
-
-    async function getJobs(){
-        try{
-            let res =  await axios.get('https://www.themuse.com/api/public/jobs?page=0&api_key=d595bf47ca0ca06d7a672a348a780469b38700e778cf80bce8ba87d4c07880fc'); 
-            console.log(res);
-        }catch(err){
-            console.log(`error : ${err}`);
-        }
+  //todo: getting data from API
+  async function getJobs(page) {
+    try {
+      let res = await axios.get(
+        `https://www.themuse.com/api/public/jobs?page=${page}&api_key=d595bf47ca0ca06d7a672a348a780469b38700e778cf80bce8ba87d4c07880fc`
+      );
+      setJobs(res.data.results);
+    } catch (err) {
+      console.log(`error : ${err}`);
     }
+  }
 
-    useEffect(()=>{
-        getJobs();
-    }, [])
+  //todo: handelling paginatoion
+  const prevPage =() =>{
+    setPage(prevPage=> prevPage-1)
+  }
+
+  const nextPage =() =>{
+    setPage(prevPage=> prevPage+1)
+  }
+
+  useEffect(() => {
+    getJobs(page);
+  }, [page]);
 
   return (
-    <div className='job_contain'>
-        <div className="image_container">
-            <h1>Companies hiring remotely in 2024</h1>
-            <p>Find below the most updated remote companies list looking for remote employees.</p>
+    <div className="job_contain">
+      <div className="job_store">
+        {jobs.map((item) => {
+          return (
+            <div className="job_card" key={item.id}>
+              <p className="job_name">{item.name}</p>
+              <p className="co_name">{item.company?.name}</p>
+              <div className="area_boxes">
+                <p className="location">
+                  <i className="fa-solid fa-location-dot"></i>
+                  {item.locations[0]?.name}
+                </p>
+                <p className="catagory">
+                  <i className="fa-brands fa-readme"></i>
+                  {item?.categories[0]?.name}
+                </p>
+                <p className="level">
+                  <i className="fa-solid fa-briefcase"></i>
+                  {item.levels[0]?.name}
+                </p>
+              </div>
+              <a href={item.refs.landing_page} target="_blank">
+                <button>
+                  Easy Apply <i class="fa-solid fa-location-arrow"></i>
+                </button>
+              </a>
+            </div>
+          );
+        })}
+        <div className="pagination">
+        <button onClick={prevPage} title='Go to previous page' disabled={page === 0}>
+        <i class="fa-solid fa-reply"></i>
+          </button>
+          <span>{page}</span>
+          <button onClick={nextPage} title='Go to next page' disabled={page === 15469}><i class="fa-solid fa-share"></i></button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default JobCtx;
